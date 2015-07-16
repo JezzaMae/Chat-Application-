ccdd7458e4044598c6e79984885bf72d5ff7a67e
JezzaMae <jezzamaecabanelez@gmail.com>
2015-07-15 21:49:24 -0700

    Done Activity02

--- client/templates/chat-template.js
+++ client/templates/chat-template.js
@@ -1,26 +1,35 @@
-Template.appChat.helpers({
-
-	messages:function(){
-		return Messages.find({}, {sort: {createdAt: -1}, limit: 10})
+Template.appProfile.helpers({
+	messages: function (){
+		return Messages.find({}, {sort:{createdAt: -1}, limit: 10})
 	}
-});
 
-Template.appChat.events({
-	"click .btnDelete": function (event){
-		Messages.remove(this._id); 
+});
+Template.appProfile.events({
+
+	"click .btnDelete": function (event) {
+		Messages.remove(this._id);
 		return false;
 	},
 	"keyup .txtText": function (event){
-		if (event.keyCode == 13){
+		if(event.keyCode == 13){
 			Messages.insert({
-				name: $(".txtName").val(),
+				StudentNo: $(".txtStudentNo").val(),
+				Course: $(".txtCourse").val(),
+				Name: $(".txtName").val(),
+				DateOfBirth: $(".txtDateOfBirth").val(),
+				CivilStatus: $(".txtCivilStatus").val(),
+				Gender: $(".txtGender").val(),
+				BloodType: $(".txtBloodType").val(),
+				Height: $(".txtHeight").val(),
+				Weight: $(".txtWeight").val(),
 				text: $(".txtText").val(),
 				createdAt: new Date()
 			});
 
-			$(".txtName").val("");
-			$(".txtName").focus();
+			$(".txtText").val("");
+			$(".txtText").focus();
 		}
 		return false;
+
 	}
 });
